4cb8d2cbe1ca6184ac91bae9798f17c6db8d02ce
Supul Kalhara <supulkalhara7@gmail.com>
2022-12-31 19:37:18 +0530

    remove people

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,10 +1,31 @@
+import { useState } from "react";
+
+
 const List = ({ allPeople }) => {
+
+  const [people, setPeople] = useState(people);
+
+  const removePeople = () => {
+      setPeople((current) =>
+      current.filter((people) => people!== people)
+    );
+  }
+
   return (
     <>
       {allPeople && allPeople.length ? (
         <ol>
           {allPeople.map((people, index) => (
-            <li key={index}> {people}</li>
+            <li key={index}> 
+              {people} 
+              <button 
+                style={{backgroundColor: 'red'}} 
+                type="button"
+                onClick={removePeople}
+              >
+                remove 
+              </button>
+            </li>
           ))}
         </ol>
       ) : (
